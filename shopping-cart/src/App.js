edd1db75bd6ec96780bc2362e2de0fc6f6e45364
Daniel <dmilano857@gmail.com>
2021-08-07 13:25:12 -0600

    updated dependencies

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Cart from './components/Cart'
-import Footer from './components/Footer'
+import ContactForm from './components/ContactForm';
 
 class App extends Component {
   render() {
@@ -15,7 +15,7 @@ class App extends Component {
                     <Route exact path="/" component={Home}/>
                     <Route path="/cart" component={Cart}/>
                   </Switch>
-                  <Footer/>
+                  <ContactForm/>
              </div>
        </BrowserRouter>
       
