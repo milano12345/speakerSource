cbde2fedba8e3682160afd070e9b3855d2d36a7f
Daniel <dmilano857@gmail.com>
2021-08-07 15:08:47 -0600

    added backend connected form, added dotenv

---
+++ shopping-cart/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+require('dotenv').config()
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => console.log("Server Running"));
+
+const contactEmail = nodemailer.createTransport({
+    service: 'gmail',
+    host: "smtp.example.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.DB_EMAIL,
+      pass: process.env.DB_PASS,
+    },
+  });
+  
+  contactEmail.verify((error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Ready to Send");
+    }
+  });
+
+  router.post("/contact", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message; 
+    const mail = {
+      from: name,
+      to: process.env.DB_EMAIL,
+      subject: "Contact Form Submission",
+      html: `<p>Name: ${name}</p>
+             <p>Email: ${email}</p>
+             <p>Message: ${message}</p>`,
+    };
+    contactEmail.sendMail(mail, (error) => {
+      if (error) {
+        res.json({ status: "ERROR" });
+      } else {
+        res.json({ status: "Message Sent" });
+      }
+    });
+  });
