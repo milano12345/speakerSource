09efd1029284f8821fa2d89fc6ee7256ada22d9e
Daniel <dmilano857@gmail.com>
2021-08-13 11:51:37 -0600

    changed endpoint

--- shopping-cart/src/components/ContactForm.js
+++ shopping-cart/src/components/ContactForm.js
@@ -11,7 +11,7 @@ const ContactForm = () => {
       email: email.value,
       message: message.value,
     };
-    let response = await fetch("https://now-express-liard.vercel.app/contact", {
+    let response = await fetch("https://senditnow.vercel.app/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
